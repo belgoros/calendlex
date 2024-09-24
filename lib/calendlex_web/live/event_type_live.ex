43a546c4faf1bfbc2a5a1f78e3ac5c5e7c2ba3d5
belgoros <s.cambour@gmail.com>
2024-09-24 12:08:11 +0200

    Update mount to display the event or not_found

--- lib/calendlex_web/live/event_type_live.ex
+++ lib/calendlex_web/live/event_type_live.ex
@@ -2,7 +2,18 @@ defmodule CalendlexWeb.EventTypeLive do
   @moduledoc false
   use CalendlexWeb, :live_view
 
-  def mount(%{"event_type_slug" => _slug} = _params, _session, socket) do
-    {:ok, socket}
+  def mount(%{"event_type_slug" => slug}, _session, socket) do
+    case Calendlex.get_event_type_by_slug(slug) do
+      {:ok, event_type} ->
+        socket =
+          socket
+          |> assign(event_type: event_type)
+          |> assign(page_title: event_type.name)
+
+        {:ok, socket}
+
+      {:error, :not_found} ->
+        {:ok, socket, layout: {CalendlexWeb.Layouts, :not_found}}
+    end
   end
 end
