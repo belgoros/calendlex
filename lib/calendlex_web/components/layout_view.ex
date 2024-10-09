1e13e1d4180921c83a2002697527afd81981cd27
belgoros <s.cambour@gmail.com>
2024-10-09 21:14:02 +0200

    Create LayoutView

---
+++ lib/calendlex_web/components/layout_view.ex
@@ -0,0 +1,14 @@
+defmodule CalendlexWeb.LayoutView do
+  use CalendlexWeb, :html
+  embed_templates "layouts/*"
+
+  alias CalendlexWeb.LiveViewHelpers
+
+  def admin_nav_link_classes(is_current) do
+    LiveViewHelpers.class_list([
+      {"py-6 font-medium text-gray-400 border-b-2 border-white hover:border-gray-400 hover:text-gray-600",
+       true},
+      {"text-gray-600 border-blue-500 hover:text-gray-600 hover:border-blue-500", is_current}
+    ])
+  end
+end
